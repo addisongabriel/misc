721af50bc4d4d9cff96b0fab530fe6781acd030f
addisongabriel <100374466+addisongabriel@users.noreply.github.com>
2023-08-18 11:05:02 -0500

    Update modiphy-footer.js

--- modiphy-footer.js
+++ modiphy-footer.js
@@ -1,6 +1,6 @@
 function generateCopyright(companyName, textColor) {
     const currentYear = new Date().getFullYear();
-    const copyrightText = `© ${currentYear} ${companyName} • All rights reserved`;
+    const copyrightText = `© ${currentYear} ${companyName} • All rights reserved | Build by <a href="https://www.modiphy.com/">Modiphy</a>`;
     const style = `color: ${textColor};`;
 
     const footerElement = document.getElementById('modiphy-footer');
